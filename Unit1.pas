cba01e32c3bf116a3ae85d90873fa972d5355b73
Zhbert <zhbert@yandex.ru>
2020-03-20 22:24:50 +0300

    Added Form Elements

--- Unit1.pas
+++ Unit1.pas
@@ -4,10 +4,15 @@ interface
 
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs;
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, Vcl.Buttons;
 
 type
   TForm1 = class(TForm)
+    GroupBox1: TGroupBox;
+    StartButton: TButton;
+    StopButton: TButton;
+    GroupBox2: TGroupBox;
+    BitBtn1: TBitBtn;
   private
     { Private declarations }
   public
